57e94ff88996c26b60d9c145162a4cae76e249db
Saksham <chauhanssaksham@gmail.com>
2020-04-29 01:49:05 +0530

    static bars created

--- src/components/SortingVisualizer/SortingVisualizer.js
+++ src/components/SortingVisualizer/SortingVisualizer.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState, useLayoutEffect} from 'react'
+import './SortingVisualizer.css'
+
+const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random()*(max - min + 1) + min);
+}
 
 const SortingVisualizer = () => {
+
+    const [array, setArray] = useState([]);
+
+    useLayoutEffect(()=>{
+        resetArray();
+    }, []);
+    
+    const resetArray = () => {
+        const arr = [];
+        for (let i=0; i<100; i++){
+            arr.push(randomIntFromInterval(5,1000));
+        }
+        setArray(arr);
+    }
+
+
     return (
-        <div>
-            Hello
+        <div className="array-container">
+            {array.map((value, idx) => (
+                <div className="array-bar" key={idx} style={{height: `${value}px`}}>
+                </div>
+            ))}
         </div>
     )
 }
